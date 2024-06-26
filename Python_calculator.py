588baf191debbc7e2c006a4f7538e6d5685c61ea
chamuthu <chamuthupabasara535@gmail.com>
2024-06-26 07:46:44 +0530

    Created Python calculator

---
+++ Python_calculator.py
@@ -0,0 +1,133 @@
+
+##All the calculation operations 
+def add(a, b):
+    return a + b
+
+def substract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+##exception error when zero division is performed
+def divide(a, b):
+    try:
+        return a / b
+    except Exception as e:
+        print("float division by zero")
+
+
+def power(a, b):
+    return a ** b
+
+def remainder(a, b):
+    return a % b
+
+
+## defined the select option and check with almost all the possiblities
+def select_op(choice):
+    if choice == '#':
+        return -1
+
+    if choice == "$":
+        return 1
+
+    elif choice in ('+', '-', '*', '/','^','%'):
+
+        # taking the 1st number for the user and making sure it is a number
+        while True:
+            first_number = input("Enter first number : ")
+            print(first_number)
+
+            reset_parameter = str(first_number).find("$")  #to check $ symbol there to reset
+            exit_parameter  = str(first_number).find("#")  #to check # symbol there to exit
+
+            if  reset_parameter != -1:
+                break
+
+            if exit_parameter != -1:
+                break
+
+            try:
+                first_number_F = float(first_number)
+                break
+
+            except:
+                print("Invalid number ! , please enter again !")
+                continue
+
+
+        # taking the 2nd number for the user and making sure it is a number
+        while (reset_parameter == -1 and exit_parameter == -1):
+            second_number = input("Enter second number : ")
+            print(second_number)
+
+            reset_parameter = str(second_number).find("$") #to check $ symbol there to reset
+            exit_parameter = str(second_number).find("#")  # to check # symbol there to exit
+
+            if reset_parameter != -1:
+                break
+
+            if exit_parameter != -1:
+                break
+
+            try:
+                second_number_F = float(second_number)
+                break
+            except:
+                print("Invalid number ! , please enter again !")
+                continue
+
+        if reset_parameter != -1:
+            return 1 #  exit from the if close because of reset option $
+
+        if exit_parameter != -1:
+            return -1 # exit from the if close because of reset option $
+
+        else:
+        #operation for the given two numbers
+            if choice == "+":
+                print(first_number_F, " + ", second_number_F, " = ", add(first_number_F,second_number_F))
+
+            if choice == "-":
+                print(first_number_F, " - ", second_number_F, " = ", substract(first_number_F,second_number_F))
+
+            if choice == "*":
+                print(first_number_F, " * ", second_number_F, " = ", multiply(first_number_F,second_number_F))
+
+            if choice == "/":
+                print(first_number_F, " / ", second_number_F, " = ", divide(first_number_F,second_number_F))
+
+            if choice == "^":
+                print(first_number_F, " * ", second_number_F, " = ", power(first_number_F,second_number_F))
+
+            if choice == "%":
+                print(first_number_F, " * ", second_number_F, " = ", remainder(first_number_F,second_number_F))
+
+           
+               
+
+    else:
+        print("Invalid operation !")
+
+
+# main loop.
+
+while True:
+    print("Select operation.")
+    print("1.Add      : + ")
+    print("2.Subtract : - ")
+    print("3.Multiply : * ")
+    print("4.Divide   : / ")
+    print("5.Power    : ^ ")
+    print("6.Remainder: % ")
+    print("7.Terminate: # ")
+    print("8.Reset    : $ ")
+
+    # take input from the user
+    choice = input("Enter choice (+ , - , * , / , ^ , % , # , $ ): ")
+    print(choice)
+    if (select_op(choice) == -1):
+        # program ends here
+        print("Done.Terminating ")
+        exit()
